cbc3edaee2dab95ca27154f74c96a3063dba0a76
Mounika Vaddepally <vaddepallymounika@gmail.com>
2021-05-19 00:15:13 +0530

    ThinkDridge Test Project - Shop Bridge Product Module Service

---
+++ ShopBridgeSolution/ProductService.Tests/Controllers/ProductControllerTest.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProductDataAccess;
+using ProductService.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductService.Tests.Controllers
+{
+    [TestClass]
+    public class ProductControllerTest
+    {
+        [TestMethod]
+        public void Get()
+        {
+            // Arrange
+            ProductController controller = new ProductController();
+
+            // Act
+            var result = controller.Get();
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void GetById()
+        {
+            // Arrange
+            ProductController controller = new ProductController();
+
+            try
+            {
+                // Act
+                var result = controller.Get(5);
+
+                // Assert
+                Assert.IsNotNull(result);
+            }
+            catch(Exception e)
+            {
+                //Do Nothing
+            }
+        }
+
+        [TestMethod]
+        public void Post()
+        {
+            // Arrange
+            ProductController controller = new ProductController();
+
+            try
+            {
+                // Act
+                controller.Post(new Product() { Name = "Dummy Product", Description = "Dummy Product", Price = 100, ProductID = 1 });
+            }
+            catch(Exception e)
+            {
+                //Do Nothing
+            }
+        }
+                
+        [TestMethod]
+        public void Delete()
+        {
+            // Arrange
+            ProductController controller = new ProductController();
+
+            // Act
+            var result = controller.Delete(1);
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
---
+++ ShopBridgeSolution/ProductService/Controllers/ProductController.cs
@@ -0,0 +1,92 @@
+﻿using ProductDataAccess;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace ProductService.Controllers
+{
+    public class ProductController : ApiController
+    {
+        public async Task<IEnumerable<Product>> Get()
+        {
+            using (ShopBridgeEntities entities = new ShopBridgeEntities())
+            {
+                List<Product> products = await entities.Products.ToListAsync();
+                return products;
+            }
+        }
+
+        public HttpResponseMessage Get(int id)
+        {
+            using (ShopBridgeEntities entities = new ShopBridgeEntities())
+            {
+                var entity = entities.Products.FirstOrDefault(p => p.ProductID == id);
+
+                if (entity != null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, entity);
+                }
+                else
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Product with Id = " + id.ToString() + " not found");
+                }
+            }
+        }
+
+        public HttpResponseMessage Post([FromBody] Product product)
+        {
+            try
+            {
+                using (ShopBridgeEntities entities = new ShopBridgeEntities())
+                {
+                    entities.Products.Add(product);
+                    entities.SaveChanges();
+
+                    var message = Request.CreateResponse(HttpStatusCode.Created, product);
+                    message.Headers.Location = new Uri(Request.RequestUri + product.ProductID.ToString());
+                    return message;
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        public async Task<HttpResponseMessage> Delete(int id)
+        {
+            try
+            {
+                using (ShopBridgeEntities entities = new ShopBridgeEntities())
+                {
+                    var entity = entities.Products.FirstOrDefault(p => p.ProductID == id);
+                    if (entity == null)
+                    {
+                        return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Product with Id =" + id.ToString() + " not found to delete");
+                    }
+                    else
+                    {
+                        entities.Products.Remove(entity);
+                        await entities.SaveChangesAsync();
+                        return Request.CreateResponse(HttpStatusCode.OK);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+    }
+}
